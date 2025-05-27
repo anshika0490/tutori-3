05f8300e1ac0543416a56aa45d41b4ce4ad8ec9a
anshika0490 <anshikasrivastava049@gmail.com>
2025-05-27 20:44:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Spotify");
+let songs =[
+    {songName:"Salam-E-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+     {songName:"Salam-E-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+      {songName:"Salam-E-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+       {songName:"Salam-E-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+        {songName:"Salam-E-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+         {songName:"Salam-E-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+]
+let audioElement = new Audio ('1.mp3');
